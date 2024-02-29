0522a21f1459c3254c3b189ccccc97c9669c6c06
andrypaul <pandriamanorintsoa@pivotworks.org>
2024-02-29 16:21:38 +0300

    merge dev branch

---
+++ .d2/shell/src/D2App/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { DataQuery } from '@dhis2/app-runtime';
+import i18n from '@dhis2/d2-i18n';
+import classes from './App.module.css';
+const query = {
+  me: {
+    resource: 'me'
+  }
+};
+const MyApp = () => /*#__PURE__*/React.createElement("div", {
+  className: classes.container
+}, /*#__PURE__*/React.createElement(DataQuery, {
+  query: query
+}, _ref => {
+  let {
+    error,
+    loading,
+    data
+  } = _ref;
+  if (error) return /*#__PURE__*/React.createElement("span", null, "ERROR");
+  if (loading) return /*#__PURE__*/React.createElement("span", null, "...");
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, i18n.t('Hello {{name}}', {
+    name: data.me.name
+  })), /*#__PURE__*/React.createElement("h3", null, i18n.t('Welcome to DHIS2!')));
+}));
+export default MyApp;
