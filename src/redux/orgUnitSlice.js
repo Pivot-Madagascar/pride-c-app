0f9d1689a8da21b6b123c1cc502f9b58e1023a21
andrypaul <pandriamanorintsoa@pivotworks.org>
2024-05-30 17:56:13 +0300

    add store

---
+++ src/redux/orgUnitSlice.js
@@ -0,0 +1,100 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const generateFormattedArray = (objects) => {
+    return objects.map((obj) => ({
+        municipality: obj.parent?.parent?.displayName,
+        municipalityId: obj.parent?.parent?.id,
+        formationSanitaire: obj.parent?.displayName,
+        formationSanitaireId: obj.parent?.id,
+        displayName: obj.displayName,
+        id: obj.id,
+    }));
+};
+
+const groupObjectsByParent = (objects) => {
+    let groupedObjects = {};
+    let parentsSet = new Set();
+
+    // Iterate through the array of objects
+    objects.forEach((obj) => {
+        // Extract the parent information
+        const parentId = obj.parent?.parent?.id;
+        const parentDisplayName = obj.parent?.parent?.displayName;
+
+        // Check if the parent information is present
+        if (parentId && parentDisplayName) {
+            // Create a unique key for the parent
+            const parentKey = `${parentDisplayName}-${parentId}`;
+
+            // Add the parent to the set
+            parentsSet.add(
+                JSON.stringify({
+                    displayName: parentDisplayName,
+                    id: parentId,
+                })
+            );
+
+            // If the group for this parent does not exist, create it
+            if (!groupedObjects[parentKey]) {
+                groupedObjects[parentKey] = {
+                    parent: {
+                        displayName: parentDisplayName,
+                        id: parentId,
+                    },
+                    combinedChildren: [],
+                };
+            }
+
+            // Add a copy of the object without the parent structure to the combinedChildren array
+            const newObj = { ...obj };
+            delete newObj.parent;
+            groupedObjects[parentKey].combinedChildren.push(newObj);
+        }
+    });
+
+    // Convert the set of parents to an array
+    const parentsArray = Array.from(parentsSet).map((parent) =>
+        JSON.parse(parent)
+    );
+
+    // Generate formatted array from objects
+    const groupedArray = generateFormattedArray(objects);
+
+    return {
+        parents: parentsArray,
+        groupedObjects: groupedArray,
+    };
+};
+
+
+
+
+const setSessionStorage = ({ key, data }) => {
+    const payload = JSON.stringify(data)
+    sessionStorage.setItem(key, payload)
+}
+
+const initialState = {
+    orgUnits: [],
+    municipalities: []
+}
+
+const orgUnitSlice = createSlice({
+    name: "orgUnit",
+    initialState,
+    reducers:{
+        setOrgUnits: (state, { payload }) => {
+            const { parents, groupedObjects } = groupObjectsByParent(payload)
+            state.orgUnits = groupedObjects
+            setSessionStorage({ key: 'orgUnits', data: groupedObjects })
+            state.municipalities = parents
+            setSessionStorage({ key: 'municipalities', data: parents })
+        }
+    },
+})
+
+export const {
+    setOrgUnits
+} = orgUnitSlice.actions
+
+export default orgUnitSlice.reducer
