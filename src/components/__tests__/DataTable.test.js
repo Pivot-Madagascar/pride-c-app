e1aa3aad8544dc327d34bab55baf2180aa9dcfa9
andrypaul <pandriamanorintsoa@pivotworks.org>
2024-08-27 14:26:47 +0300

    update DataTable test

--- src/components/__tests__/DataTable.test.js
+++ src/components/__tests__/DataTable.test.js
@@ -1,70 +1,111 @@
 import { render, screen, fireEvent } from '@testing-library/react'
 import React from 'react'
 import '@testing-library/jest-dom'
-// import { headCells } from '../config'
 import DataTable from '../DataTable'
-import { headCells } from '../DataTable/config'
+
+jest.mock('jspdf', () => {
+    return {
+        jsPDF: jest.fn(() => ({
+            save: jest.fn(),
+        })),
+    }
+})
+
+jest.mock('jspdf-autotable', () => jest.fn())
 
 // Sample data to be used in tests
 const sampleData = [
-    { id: 1, municipality: 'Municipality 1', orgUnitName: 'Fokontany 1', periodName: 'July 2016', min: 10, mean: 20, max: 30 },
-    { id: 2, municipality: 'Municipality 2', orgUnitName: 'Fokontany 2', periodName: 'July 2016', min: 15, mean: 25, max: 35 },
-    { id: 3, municipality: 'Municipality 3', orgUnitName: 'Fokontany 3', periodName: 'July 2016', min: 20, mean: 30, max: 40 },
-    { id: 4, municipality: 'Municipality 4', orgUnitName: 'Fokontany 4', periodName: 'July 2016', min: 25, mean: 35, max: 45 },
-    { id: 5, municipality: 'Municipality 5', orgUnitName: 'Fokontany 5', periodName: 'July 2016', min: 30, mean: 40, max: 50 },
-    { id: 6, municipality: 'Municipality 6', orgUnitName: 'Fokontany 6', periodName: 'July 2016', min: 35, mean: 45, max: 55 },
+    {
+        id: 1,
+        municipality: 'Municipality 1',
+        orgUnitName: 'Fokontany 1',
+        periodName: 'July 2016',
+        min: 10,
+        mean: 20,
+        max: 30,
+        period: '201607',
+    },
+    {
+        id: 2,
+        municipality: 'Municipality 2',
+        orgUnitName: 'Fokontany 2',
+        periodName: 'July 2016',
+        min: 15,
+        mean: 25,
+        max: 35,
+        period: '201607',
+    },
+    {
+        id: 3,
+        municipality: 'Municipality 3',
+        orgUnitName: 'Fokontany 3',
+        periodName: 'July 2016',
+        min: 20,
+        mean: 30,
+        max: 40,
+        period: '201607',
+    },
+    {
+        id: 4,
+        municipality: 'Municipality 4',
+        orgUnitName: 'Fokontany 4',
+        periodName: 'July 2016',
+        min: 25,
+        mean: 35,
+        max: 45,
+        period: '201607',
+    },
+    {
+        id: 5,
+        municipality: 'Municipality 5',
+        orgUnitName: 'Fokontany 5',
+        periodName: 'July 2016',
+        min: 30,
+        mean: 40,
+        max: 50,
+        period: '201607',
+    },
+    {
+        id: 6,
+        municipality: 'Municipality 6',
+        orgUnitName: 'Fokontany 6',
+        periodName: 'July 2016',
+        min: 35,
+        mean: 45,
+        max: 55,
+        period: '201607',
+    },
 ]
 
 describe('DataTable component', () => {
     it('renders DataTable with correct data', () => {
-        const { getByText } = render(<DataTable data={sampleData} />)
+        render(<DataTable data={sampleData} />)
 
-        // Check if table headers are rendered
-        headCells.forEach((headCell) => {
-            expect(getByText(headCell.label)).toBeInTheDocument()
-        })
+        expect(screen.getByText('Commune')).toBeInTheDocument()
+        expect(screen.getByText('Fokontany')).toBeInTheDocument()
+        expect(screen.getByText('Mois')).toBeInTheDocument()
+        expect(screen.getByText('Estimation min.')).toBeInTheDocument()
+        expect(screen.getByText('Estimation moyenne')).toBeInTheDocument()
+        expect(screen.getByText('Estimation max.')).toBeInTheDocument()
 
-        // Check if first page data is rendered correctly
         sampleData.slice(0, 5).forEach((item) => {
-            expect(getByText(item.municipality)).toBeInTheDocument()
+            expect(screen.getByText(item.municipality)).toBeInTheDocument()
         })
-    })
-
-    it('pagination works correctly', () => {
-        const { queryByText, getByLabelText, getByText } = render(<DataTable data={sampleData} />)
-
-        // Check initial page
-        expect(queryByText('Municipality 6')).not.toBeInTheDocument()
-
-        const nextPageButton = getByLabelText('Go to next page')
-
-        // Navigate to next page
-        fireEvent.click(nextPageButton)
-
-        // Check if next page data is rendered correctly
-        expect(getByText('Municipality 6')).toBeInTheDocument()
     })
 
     it('sorting works correctly', () => {
-        const { getByText, getAllByRole } = render(<DataTable data={sampleData} />)
+        render(<DataTable data={sampleData} />)
 
-        // Click on the "Min" column header to sort
-        fireEvent.click(getByText('Estimation minimun'))
+        fireEvent.click(screen.getByText('Estimation min.'))
+        const firstRow = screen.getAllByRole('row')[1]
+        const secondRow = screen.getAllByRole('row')[2]
+        expect(firstRow).toHaveTextContent('Municipality 6')
+        expect(secondRow).toHaveTextContent('Municipality 5')
 
-        // Check if the rows are sorted by "Min" in ascending order
-        const rows = getAllByRole('row')
-        const firstRowCells = rows[1].querySelectorAll('td')
-        const secondRowCells = rows[2].querySelectorAll('td')
-        expect(firstRowCells[3].textContent).toBe('20')
-        expect(secondRowCells[3].textContent).toBe('25')
-
-        // Click again to sort in descending order
-        fireEvent.click(getByText('Estimation minimun'))
-
-        // Check if the rows are sorted by "Min" in descending order
-        const firstRowCellsAsc = rows[1].querySelectorAll('td')
-        const secondRowCellsAsc = rows[2].querySelectorAll('td')
-        expect(firstRowCellsAsc[3].textContent).toBe('20')
-        expect(secondRowCellsAsc[3].textContent).toBe('25')
+        fireEvent.click(screen.getByText('Estimation min.'))
+        const firstRowDesc = screen.getAllByRole('row')[1]
+        const secondRowDesc = screen.getAllByRole('row')[2]
+        expect(firstRowDesc).toHaveTextContent('Municipality 1')
+        expect(secondRowDesc).toHaveTextContent('Municipality 2')
     })
 })
