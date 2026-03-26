ffc37e13f7226400a5c246906c387e73988ee6ac
andrypaul <pandriamanorintsoa@pivotworks.org>
2026-03-26 11:12:48 +0300

    Update ESLint configuration with alias resolver and rules for improved import management

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,48 @@
+const path = require('path')
 const { config } = require('@dhis2/cli-style')
 
 module.exports = {
     extends: [config.eslint],
+    settings: {
+        'import/resolver': {
+            alias: {
+                map: [
+                    ['@', path.resolve(__dirname, 'src')],
+                ],
+                extensions: ['.js', '.jsx', '.json'],
+            },
+        },
+    },
+    rules: {
+        'import/extensions': ['error', 'ignorePackages', {
+            js: 'never',
+            jsx: 'never',
+        }],
+        'import/order': ['error', {
+            groups: [
+                'builtin',
+                'external',
+                'internal',
+                ['parent', 'sibling', 'index'],
+            ],
+            pathGroups: [
+                {
+                    pattern: '@dhis2/**',
+                    group: 'external',
+                    position: 'before',
+                },
+                {
+                    pattern: '@mui/**',
+                    group: 'external',
+                    position: 'before',
+                },
+                {
+                    pattern: '@/**',
+                    group: 'internal',
+                    position: 'after',
+                },
+            ],
+            pathGroupsExcludedImportTypes: ['builtin'],
+        }],
+    },
 }
