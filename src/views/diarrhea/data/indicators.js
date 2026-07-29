502276e0d77acafc7310bdfd193ffdf723887043
andrypaul <pandriamanorintsoa@pivotworks.org>
2026-07-29 15:48:44 +0300

    Refactor disease data handling for Diarrhea, IRA, and Malaria

--- src/views/diarrhea/data/indicators.js
+++ src/views/diarrhea/data/indicators.js
@@ -1,52 +1,6 @@
-
 import { DIARRHEA } from '@/constants/mapping'
-import { getMonthYYYYMM } from '@/utils/format-time'
+import { createIndicatorData } from '@/utils/diseaseDataFactories'
 
-const getDiarrheaIndicator = () => {
-    const indicatorElements = [
-        {
-            path: ['alert', 'csb'],
-            periods: [getMonthYYYYMM()],
-            dataElement: DIARRHEA.alert.csb.id
-        },
-        {
-            path: ['alert', 'comCases'],
-            periods: [getMonthYYYYMM()],
-            dataElement: DIARRHEA.alert.comCases.id
-        },
-        {
-            path: ['alert', 'incidence'],
-            periods: [getMonthYYYYMM()],
-            dataElement: DIARRHEA.alert.incidence.id
-        },
-        {
-            path: ['alert', 'csbVigilance'],
-            periods: [getMonthYYYYMM()],
-            dataElement: DIARRHEA.alert.csbVigilance.id
-        },
-        {
-            path: ['compare', 'trend'],
-            periods: [getMonthYYYYMM()],
-            dataElement: DIARRHEA.compare.trend.id
-        },
-        {
-            path: ['compare', 'csb'],
-            periods: [getMonthYYYYMM()],
-            dataElement: DIARRHEA.compare.csb.id
-        },
-        {
-            path: ['compare', 'comCases'],
-            periods: [getMonthYYYYMM()],
-            dataElement: DIARRHEA.compare.comCases.id
-        },
-        {
-            path: ['compare', 'incidence'],
-            periods: [getMonthYYYYMM()],
-            dataElement: DIARRHEA.compare.incidence.id
-        }
-    ]
-    return {
-        indicatorElements,
-    }
-}
+const getDiarrheaIndicator = () => createIndicatorData(DIARRHEA)
+
 export default getDiarrheaIndicator
--- src/views/malaria/data/simulation.js
+++ src/views/malaria/data/simulation.js
@@ -1,64 +1,6 @@
 import { MALARIA } from '@/constants/mapping'
-import { generateYearMonths } from '@/utils/format-time'
+import { createSimulationData } from '@/utils/diseaseDataFactories'
 
-const generateSimulationPeriods = () => {
-    const currentDate = new Date()
-    const currentYear = currentDate.getFullYear()
-    const currentMonth = currentDate.getMonth()
-    const periods = []
-    for (let monthOffset = 0; monthOffset < currentMonth; monthOffset++) {
-        const monthIndex = monthOffset
-        const year = currentYear
-        const month = monthIndex
-
-        const formattedMonth = String(month + 1).padStart(2, '0')
-        const period = `${year}${formattedMonth}`
-        periods.push(period)
-    }
-
-    for (let monthOffset = 0; monthOffset <= 2; monthOffset++) {
-        const monthIndex = currentMonth + monthOffset
-        const year = currentYear + Math.floor(monthIndex / 12)
-        const month = monthIndex % 12
-
-        const formattedMonth = String(month + 1).padStart(2, '0')
-        const period = `${year}${formattedMonth}`
-        periods.push(period)
-    }
-    return periods
-}
-
-const getMalariaSimulation = () => {
-    const simulationElements = [
-        // simulation (from 2022 to 2024) -- fake data
-        {
-            dataElement: MALARIA.forecast.adjusted.avg.id,
-            path: ['simulation', 'historic'],
-            periods: [
-                ...generateYearMonths(2022),
-                ...generateYearMonths(2023),
-                ...generateYearMonths(2024),
-                ...generateYearMonths(2025),
-            ],
-        },
-        {
-            dataElement: MALARIA.forecast.adjusted.avg.id,
-            path: ['simulation', 'adjusted'],
-            periods: [...generateSimulationPeriods()],
-        },
-        {
-            dataElement: MALARIA.forecast.comCases.avg.id,
-            path: ['simulation', 'comCases'],
-            periods: [...generateSimulationPeriods()],
-        },
-        {
-            dataElement: MALARIA.forecast.csbCases.avg.id,
-            path: ['simulation', 'csbCases'],
-            periods: [...generateSimulationPeriods()],
-        },
-    ]
-
-    return { simulationElements }
-}
+const getMalariaSimulation = () => createSimulationData(MALARIA)
 
 export default getMalariaSimulation
